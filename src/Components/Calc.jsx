397c1a1b092b1aec3c6a9e4b74105f51b547ce3f
Ali Mohamed <alimohamed351@outlook.com>
2025-05-22 15:14:07 +0300

    added styles

--- src/Components/Calc.jsx
+++ src/Components/Calc.jsx
@@ -7,20 +7,55 @@ export default function Calc() {
                 <p>Clear All</p>
             </div>
 
+            {/* AMOUNT */}
             <div className="flex flex-col">
                 <label htmlFor="amnt">Mortgage Amount</label>
                 <input type="text" name="amnt" id="amnt" className="border-2 border-black" />
             </div>
+            {/* AMOUNT */}
 
-            <div className="flex flex-col">
-                <label htmlFor="amnt">Mortgage Term</label>
-                <input type="text" name="amnt" id="amnt" className="border-2 border-black" />
+            {/* TERM & RATE */}
+            <div className="flex w-full mt-10 justify-between gap-8 mb-8">
+                <div className="flex flex-col">
+                    <label htmlFor="term">Mortgage Term</label>
+                    <input type="text" name="term" id="term" className="border-2 border-black w-full" />
+                </div>
+
+                <div className="flex flex-col">
+                    <label htmlFor="rate">Interest rate</label>
+                    <input type="text" name="rate" id="rate" className="border-2 border-black w-full" />
+                </div>
+            </div>
+            {/* TERM & RATE */}
+
+            <div>
+                <div className="flex flex-col">
+                    <label htmlFor="amnt" className="text-white mb-4">Mortgage Type</label>
+                    <div className="flex gap-2 items-center">
+                        <input
+                            type="radio"
+                            id="repayment"
+                            name="repaymentType"
+                            value="repayment"
+                            className="w-4 h-4 text-blue-600 border-gray-300 focus:ring-blue-500"
+                        />
+                        <label htmlFor="repayment" className="text-gray-700">Repayment</label>
+                    </div>
+
+                    <div className="flex gap-2 items-center">
+                        <input
+                            type="radio"
+                            id="intrest"
+                            name="repaymentType"
+                            value="intrest"
+                            className="w-4 h-4 text-blue-600 border-gray-300 focus:ring-blue-500"
+                        />
+                        <label htmlFor="intrest" className="text-gray-700">Intrest only</label>
+                    </div>
+                </div>
             </div>
 
-            <div className="flex flex-col">
-                <label htmlFor="amnt">Mortgage Type</label>
-                <input type="text" name="amnt" id="amnt" className="border-2 border-black" />
-            </div>
+
 
         </div >
     );
